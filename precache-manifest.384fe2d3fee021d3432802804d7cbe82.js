c514c83d1b6426a005a1f694e7d67b8dc87b74ff
leonatwork <noelabydas@gmail.com>
2020-07-06 23:46:28 +0530

    Updates

---
+++ precache-manifest.384fe2d3fee021d3432802804d7cbe82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d58dfad05f4ab2a35b491882a015564",
+    "url": "/dbms-notes/index.html"
+  },
+  {
+    "revision": "40b90ad9a0e4b8f82a6e",
+    "url": "/dbms-notes/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "40b90ad9a0e4b8f82a6e",
+    "url": "/dbms-notes/static/js/2.200940d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dbms-notes/static/js/2.200940d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fb7ed601c0d466e37a",
+    "url": "/dbms-notes/static/js/main.8770fc5c.chunk.js"
+  },
+  {
+    "revision": "8ee2c27b31d02789a4a7",
+    "url": "/dbms-notes/static/js/runtime-main.b7165b60.js"
+  }
+]);
